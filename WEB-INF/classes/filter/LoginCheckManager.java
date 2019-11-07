8e7108cda01ef2a9a260ef6d7273dbc8adf177e1
hy-18304021 <yh18304021@ga.tera-house.ac.jp>
2019-11-07 15:38:28 +0900

    no message

--- WEB-INF/classes/filter/LoginCheckManager.java
+++ WEB-INF/classes/filter/LoginCheckManager.java
@@ -12,11 +12,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.SQLException;
-
-
 public class LoginCheckManager implements Filter{
     private FilterConfig config;
     public void init(FilterConfig config)throws ServletException{}
@@ -24,41 +19,16 @@ public class LoginCheckManager implements Filter{
     public void doFilter(ServletRequest req,ServletResponse res,FilterChain chain)
      throws IOException,ServletException{
 
-        Connection cn=null;
         //id取得
         String id=req.getParameter("name");
         //パスワード取得
         String pass=req.getParameter("pass");
 
-        cn=new OracleConnector("info","pro").getCn();
-		
-		TableReferer tr=new TableReferer(cn);
-        //EBUSERのレコード取得
-        int record=tr.getRecord();
-
-        for(int i=0;i>=record;i++){
-            //EBUSERのidとpassを取得する
-            String dbid=tr.getId(i);
-            String dbpass=tr.getPass(i);
-
-            if(dbid!=null&&dbpass!=null){
-                ///EBUSERからとってきた
-                //idとpassのチェック
-                if(id.equals(dbid)&&pass.equals(dbpass)){
-                    if(dbid=="ECBookM"&&dbpass=="manager"){
+                    if(id=="ECBookM"&&pass=="manager"){
                         //認証されたら認証トークンをセット
                         HttpSession session=((HttpServletRequest)req).getSession();
                         session.setAttribute("manager","OK");
                     }
-                }
-            }
-        }
-        tr.Trclose();
-        try{
-            cn.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
         //本来のURLへ
         chain.doFilter(req,res);
         

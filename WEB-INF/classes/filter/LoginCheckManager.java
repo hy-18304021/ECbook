407434cba05fbff86f8617a8baffa06bef06e1e2
hy-18304021 <yh18304021@ga.tera-house.ac.jp>
2019-11-14 16:17:09 +0900

    no message

--- WEB-INF/classes/filter/LoginCheckManager.java
+++ WEB-INF/classes/filter/LoginCheckManager.java
@@ -12,9 +12,11 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+
 public class LoginCheckManager implements Filter{
     private FilterConfig config;
     public void init(FilterConfig config)throws ServletException{
+        //初期化するためのinit内でインスタンス変数への格納
         this.config=config;
     }
     public void destroy(){}
@@ -26,17 +28,23 @@ public class LoginCheckManager implements Filter{
         //パスワード取得
         String pass=req.getParameter("pass");
 
+        //初期値パラメータを取得する
         String mid=config.getInitParameter("ManagerID");
         String mpss=config.getInitParameter("ManagerPass");
+
         System.out.println(mid);
         System.out.println(mpss);
-        HttpSession session=((HttpServletRequest)req).getSession();
+
+        if(id!=null&&pass!=null){
+            //初期値パラメータからとってきた
+            //ユーザー名とパスワードのチェック
             if(id.equals(mid)&&pass.equals(mpss)){
-                System.out.println("naka");
                 //認証されたら認証トークンをセット
-            
-                session.setAttribute("mToken","OK");
+                HttpSession session=((HttpServletRequest)req).getSession();
+                session.setAttribute("token","OK");
             }
+        }
+            
         //本来のURLへ
         chain.doFilter(req,res);
         

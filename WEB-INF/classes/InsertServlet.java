973053ff7971d31e1491b4c007667685e413d12b
hy-18304021 <yh18304021@ga.tera-house.ac.jp>
2019-11-14 14:17:25 +0900

    manegerの商品登録

---
+++ WEB-INF/classes/InsertServlet.java
@@ -0,0 +1,35 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import DBOracle.InsertOracle;
+import bean.SelectOracleBean;
+
+public class InsertServlet{
+    public void doGet(HttpServletRequest req,HttpServletResponse res)
+	throws IOException,ServletException{
+		
+        req.setCharacterEncoding("Windows-31J");
+
+        int book_id=req.getParamer("book_id");//本ID
+        int book_price=req.getParamer("book_price");//本値段
+        int book_count=req.getParamer("book_count");//本在庫
+        String book_image=req.getParamer("book_image");//本画像
+        String book_isbn=req.getParamer("book_isbn");//ISBNコード     
+        
+        InsertOracle.InsertBook(book_id, book_price, book_count, book_image, book_isbn);
+        
+
+		
+		//転送先のJSPを指定
+		RequestDispatcher dis=req.getRequestDispatcher("/manager");
+		
+		//パラメータをJSPに転送
+		dis.forward(req,res);
+		
+    }
+}

5b212e12ed03bbc24bf7b97450fe58ac0d8c696a
nim-oi <44827625+nim-oi@users.noreply.github.com>
2019-11-26 14:04:47 +0900

    db設計更新

--- sql/ok/StartOracle.bat
+++ sql/ok/StartOracle.bat
@@ -3,11 +3,4 @@ net start OracleVssWriterORCL
 net start OracleOraDB12Home1MTSRecoveryService
 net start OracleOraDB12Home1TNSListener
 net start OracleServiceORCL
-
-sqlplus -s / as sysdba/orcl@orcl @testusercreate.sql
-cd sql
-for %%a in (*.sql) do (
-  sqlplus -s ebtest/ebpass@orcl @%%a
-)
-
 sqlplus -s ebtest/ebpass@orcl

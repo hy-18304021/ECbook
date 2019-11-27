e9bba567057f6e35ae1c6950c492750ff2cba303
nim-oi <44827625+nim-oi@users.noreply.github.com>
2019-11-27 19:44:33 +0900

    DB設計一応完成。SQL制作中。

--- sql/ok/testDbInit.bat
+++ sql/ok/testDbInit.bat
@@ -4,10 +4,9 @@ net start OracleOraDB12Home1MTSRecoveryService
 net start OracleOraDB12Home1TNSListener
 net start OracleServiceORCL
 
-sqlplus -s / as sysdba/orcl@orcl @testusercreate.sql
 cd sql
-for %%a in (*.sql) do (
-  sqlplus -s ebtest/ebpass@orcl @%%a
+for /f %%a in (sqlfilelist.txt) do (
+  sqlplus -s ebtest/ebpass@orcl @%%a.sql
 )
 
 sqlplus -s ebtest/ebpass@orcl

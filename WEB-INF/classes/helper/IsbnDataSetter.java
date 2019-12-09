65192e27c3cda2af0517bddc0848b9251ad9638f
nim-oi <44827625+nim-oi@users.noreply.github.com>
2019-12-09 21:59:42 +0900

    isbn入ったBookBean渡すとisbnから取得できるデータ詰めて返すclass

--- WEB-INF/classes/helper/IsbnDataSetter.java
+++ WEB-INF/classes/helper/IsbnDataSetter.java
@@ -3,11 +3,15 @@ package helper;
 import com.fasterxml.jackson.databind.JsonNode;
 import bean.EBBookBean;
 public class IsbnDataGetter{
-
-    public static EBBookBean execute(EBBookBean eb){
+    //jsonからデータ取り出してBeanにデータ入れる。
+    public static EBBookBean getIsbnData(EBBookBean eb){
         ObjectMapper mapper = new ObjectMapper();
         try {
-            JsonNode node = mapper.readTree(json);
+            //Beanからisbn取り出し
+            String isbn=eb.getBook_isbn();
+            //isbn使ってjson取得。openBDのAPI使ってるクラスのメソッド。
+            JsonNode node = IsbnConverter.IsbnConvert(isbn);
+
 
             String name = node.get("onix").get("DescriptiveDetail").get("TitleDetail").get("TitleElement").get("TitleText").get("content").asText();
             eb.setBook_name(name);
@@ -15,17 +19,38 @@ public class IsbnDataGetter{
             eb.setPublisher(publisher);
             String series=node.get("onix").get("summary").get("series").asText();
             eb.setSeries(series);
-            int volume=node.get("onix").get("summary").get("volume");
+            int volume=node.get("onix").get("summary").get("volume").asInt();
             eb.setVolume(volume);
-            String author=.get("Contributor").get("ContributorRole":["A01"],"PersonName").get("content").asText();
+
+            //著者。ContributorRoleが[A01]のものを選んで取得。他の選べばイラストレーターとか訳者とかとれる。
+            JsonNode contributors=node.get("onix").get("DescriptiveDetail").get("Contributor");
+            String author=null;
+            if(contributors.get("ContributorRole").get(0).asText().equals("A01")){
+                author=contributors.get("PersonName").get("content");
+            }
             eb.setAuthor(author);
-            int release_date=.get("onix").get("summary").get("pubdate");
+
+
+            int release_date=node.get("onix").get("summary").get("pubdate").asInt();
             eb.setRelease_date(release_date);
-            String audience=.get("Audience");
+
+            //対象。今回は成人指定か否かを取得。"AudienceCodeType":"22"あったら成人指定。細かい理由欲しければ"AudienceCodeValue"も見る
+            JsonNode audienceNode=node.get("onix").get("DescriptiveDetail")get("Audience");
+            String audience=null;
+            if(audienceNode.get("AudienceCodeType").asText().equals("22")){
+                audience="成人向け";
+            }
             eb.setAudience(audience);
-            String label=.get("onix").get("DescriptiveDetail").get("Collection").get("TitleDetail").get("TitleElement").get("TitleText").get("content").asText();
+
+            String label=node.get("onix").get("DescriptiveDetail").get("Collection").get("TitleDetail").get("TitleElement").get("TitleText").get("content").asText();
             eb.setLabel(label);
-            String text_content=.get("CollateralDetail").get("TextContent").asText();
+
+            //TextType:02なら略説。TextType:03なら詳説。詳説を選択したが、サイズでかすぎたら考える。
+            JsonNode textNode=node.get("onix").get("CollateralDetail").get("TextContent").asText();
+            String text_content=null;
+            if(textNode.get("TextType").asText().equals("03")){
+                text_content=textNode.get("Text").asText();
+            }
             eb.setText_content(text_content);
 
 

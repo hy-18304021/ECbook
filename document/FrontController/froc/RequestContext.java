2b6c37cdd46173d48426ecb8c95fc25fc861dbef
hy-18304021 <yh18304021@ga.tera-house.ac.jp>
2019-12-11 14:46:06 +0900

    no message

--- document/FrontController/froc/RequestContext.java
+++
@@ -1,9 +0,0 @@
-package froc;
-
-public interface RequestContext{
-    public String getCommandPath();
-    public String[] getParameter(String key);
-    public Object getSession();
-    public Object getRequest();
-    public void setRequest(Object request);
-}

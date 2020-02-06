2bd3dc6a1e9b1002a7c3421cb103ca310cedf593
nim-oi <44827625+nim-oi@users.noreply.github.com>
2020-02-06 14:59:18 +0900

    テストデータでebbookテーブルに説明文が正常にinsertされるように

--- sql/ok/testDataInsert.bat
+++ sql/ok/testDataInsert.bat
@@ -1,6 +1,14 @@
 call StartOracle.bat
 cd loader/ldr
-
-cd sql
-sqlplus -s ebtest/ebpass@orcl @insert.sql
+sqlldr ebtest/ebpass@orcl control='EBUSER_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBADDRESS_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBCREDIT_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBGENRE_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBBOOK_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBCART_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBFAVORITE_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBREVIEW_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBSALES_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBSALES_REF_DATA_TABLE.ctl'
+sqlldr ebtest/ebpass@orcl control='EBARRIVAL_DATA_TABLE.ctl'
 sqlplus -s ebtest/ebpass@orcl

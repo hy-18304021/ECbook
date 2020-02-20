ec80a640a495f7950647a0b7fb5f7db6d3acdb74
susu1996 <44827526+susu1996@users.noreply.github.com>
2020-02-20 13:56:29 +0900

    no message

--- WEB-INF/classes/filter/RecommandedBookFilter.java
+++ WEB-INF/classes/filter/RecommandedBookFilter.java
@@ -17,11 +17,11 @@ import froc.*;
 import dao.*;
 import java.util.List;
 
-public class MainPageFilter implements Filter{
+public class RecommandedBookFilter implements Filter{
     public void init(FilterConfig config)throws ServletException{}
     public void destroy(){}
     public void doFilter(ServletRequest req,ServletResponse res,FilterChain chain)throws IOException,ServletException{
-        // System.out.println("MainPageFilter");
+        // System.out.println("RecommandedBookFilter");
         HttpServletRequest hreq = (HttpServletRequest)req;
         RequestContext reqc = new WebRequestContext();
         reqc.setRequest(hreq);
@@ -32,6 +32,14 @@ public class MainPageFilter implements Filter{
         List recommendedBooks = bookdao.getRecommendedBooks();
         hreq.setAttribute("recommendedBooks",recommendedBooks);
 
+        List recommendedShounen = bookdao.getRecommendedBooks(1);
+        hreq.setAttribute("recommendedShounen",recommendedShounen);
+
+        List recommendedShoujo = bookdao.getRecommendedBooks(2);
+        hreq.setAttribute("recommendedShoujo",recommendedShoujo);
+
+        List recommendedLightNovel = bookdao.getRecommendedBooks(3);
+        hreq.setAttribute("recommendedLightNovel",recommendedLightNovel);
 
         chain.doFilter(req,res);
     }
